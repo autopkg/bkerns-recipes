dc21141b4389d60e22fda00cfde95a379cd5255f
Elliot Jordan <elliot@elliotjordan.com>
2019-08-31 21:51:02 -0700

    Adjust urllib import

--- TechSmithCamtasia/TechSmithURLProvider.py
+++ TechSmithCamtasia/TechSmithURLProvider.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
 
 from __future__ import absolute_import
-import urllib
+
 import json
 
 from autopkglib import Processor, ProcessorError
+
+try:
+    from urllib import request as urllib  # For Python 3
+except ImportError:
+    import urllib  # For Python 2
+
 
 
 __all__ = ["TechSmithURLProvider"]
@@ -59,4 +65,3 @@ class TechSmithURLProvider(Processor):
 if __name__ == "__main__":
     PROCESSOR = TechSmithURLProvider()
     PROCESSOR.execute_shell()
-
