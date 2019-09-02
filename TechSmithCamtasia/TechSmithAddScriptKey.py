ca5ad2bc13883cce392043afb5efc1785eae305d
Elliot Jordan <elliot@elliotjordan.com>
2019-09-01 21:22:33 -0700

    Sort imports using isort and remove unused imports

--- TechSmithCamtasia/TechSmithAddScriptKey.py
+++ TechSmithCamtasia/TechSmithAddScriptKey.py
@@ -14,12 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from __future__ import absolute_import
-from __future__ import print_function
+from __future__ import absolute_import, print_function
+
 import os
 
-from autopkglib import Processor, ProcessorError
-from autopkglib import get_pref
+from autopkglib import Processor, ProcessorError, get_pref
 
 __all__ = ["TechSmithAddScriptKey"]
 
